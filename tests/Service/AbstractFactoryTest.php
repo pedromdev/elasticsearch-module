c428dd209f8d9bf3ed5efc06f3e4605339a1ee0b
Pedro Alves <pedromarcelodesaalves@gmail.com>
2017-02-27 00:37:20 -0300

    Updated: Changing the methods location

--- tests/Service/AbstractFactoryTest.php
+++ tests/Service/AbstractFactoryTest.php
@@ -2,49 +2,9 @@
 
 namespace ElasticsearchModuleTest\Service;
 
-use PHPUnit_Framework_MockObject_MockObject;
-use Zend\ServiceManager\ServiceLocatorInterface;
-
 /**
  * @author Pedro Alves <pedro.m.develop@gmail.com>
  */
 abstract class AbstractFactoryTest extends AbstractTest
 {
-    
-    /**
-     * @param array $methods
-     * @return PHPUnit_Framework_MockObject_MockObject
-     */
-    protected function createServiceLocatorMock(array $methods)
-    {
-        return $this->getMockBuilder(ServiceLocatorInterface::class)
-            ->setMethods($methods)
-            ->disableOriginalConstructor()
-            ->getMockForAbstractClass();
-    }
-    
-    /**
-     * @param PHPUnit_Framework_MockObject_MockObject $mock
-     */
-    protected function mockConfigurationService(PHPUnit_Framework_MockObject_MockObject $mock)
-    {
-        $mock->expects($this->any())
-            ->method('get')
-            ->with($this->equalTo('Config'))
-            ->willReturn($this->getConfig());
-    }
-    
-    /**
-     * @param PHPUnit_Framework_MockObject_MockObject $mock
-     * @param string $method
-     * @param array $map
-     */
-    protected function mockMappedReturn(PHPUnit_Framework_MockObject_MockObject $mock, $method, array $map)
-    {
-        $mock->expects($this->any())
-            ->method($method)
-            ->willReturnCallback(function($name) use ($map) {
-                return isset($map[$name]) ? $map[$name] : null;
-            });
-    }
 }
